594e18ceb7335d4add5bce848f309160980eb050
Gabriel Tomazi <tomazigabriel3@gmail.com>
2026-07-20 23:36:31 -0300

    fix: corrige PrismaNeon adapter com WebSocket e adiciona @types/ws

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,9 +1,12 @@
-import 'dotenv/config'
+import { neonConfig } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
+import 'dotenv/config'
+import ws from 'ws'
 import { PrismaClient } from '../generated/client'
 
-const adapter = new PrismaNeon({
-  connectionString: process.env.DATABASE_URL,
-})
+neonConfig.webSocketConstructor = ws
+
+const connectionString = process.env.DATABASE_URL!
+const adapter = new PrismaNeon({ connectionString })
 
 export const prisma = new PrismaClient({ adapter })
