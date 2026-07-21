e08e7a35f155fd98774f67569ff19a0d2b09a018
Gabriel Tomazi <tomazigabriel3@gmail.com>
2026-07-20 23:39:06 -0300

    refactor: simplifica inicializacao do PrismaNeon e remove pacote ws

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,12 +1,8 @@
-import { neonConfig } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
 import 'dotenv/config'
-import ws from 'ws'
 import { PrismaClient } from '../generated/client'
 
-neonConfig.webSocketConstructor = ws
-
-const connectionString = process.env.DATABASE_URL!
+const connectionString = process.env.DATABASE_URL
 const adapter = new PrismaNeon({ connectionString })
 
 export const prisma = new PrismaClient({ adapter })
